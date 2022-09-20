93a48bcebb4f43f670d57129e73438a4cab3a69b
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-09-20 15:06:00 +0200

    v2.0

---
+++ 1.4/Source/VanillaWeaponsExpandedNonLethal/VanillaWeaponsExpandedNonLethal/DamageWorker_AddInjury_NoMechs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using RimWorld;
+using Verse;
+using UnityEngine;
+
+
+
+namespace VanillaWeaponsExpandedNonLethal
+{
+    public class DamageWorker_AddInjury_NoMechs : DamageWorker_AddInjury
+    {
+
+
+
+        public override DamageResult Apply(DamageInfo dinfo, Thing thing)
+        {
+            Pawn pawn = thing as Pawn;
+            if (pawn?.RaceProps?.IsMechanoid == true)
+            {
+                DamageResult damageResult = new DamageResult();
+                return damageResult;
+            }
+            else return base.Apply(dinfo, thing);
+
+
+        }
+
+
+
+
+    }
+
+
+}
+
+
+
